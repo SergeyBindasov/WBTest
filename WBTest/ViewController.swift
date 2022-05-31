f2f08082188e6f8e5438d3dd345db57d240f41f9
Sergey <bindasov.sergey@gmail.com>
2022-05-31 11:44:31 +0300

    Initial commit

---
+++ WBTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WBTest
+//
+//  Created by Sergey on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
